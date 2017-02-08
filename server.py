bc2510e136c70fa6660cb031528fc4b4b70afdeb
Olivia Stalcup <ocstalcup@gmail.com>
2017-02-08 14:22:08 -0800

    added ninja button

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, session 
+from flask import Flask, render_template, session, redirect 
 app = Flask(__name__)
 app.secret_key = "specialKeyChippers"
 
 @app.route('/')
 def index():
-
 	if 'count' not in session:
 		session['count'] = 1
 	else:
 		session['count'] += 1	 
 	return render_template('index.html')
 
+@app.route('/ninja')
+def clicked():
+	session['count'] += 1
+	return redirect('/')
 app.run(debug=True)
